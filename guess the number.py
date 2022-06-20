6121f7f70349aff1436aca397574875ce4ff3629
actionjackson7 <sydneyleiladev@gmail.com>
2022-06-20 19:45:15 -0400

    yeah

---
+++ guess the number.py
@@ -0,0 +1,36 @@
+#guess the number(computer)
+
+from ast import Expression
+import random
+
+def guess(x):
+    random_number = random.randint(1, x)
+    
+    guess = 0
+    while guess != random_number:
+        guess = int(input(f"Guess a number between 1 and {x}: "))
+        if guess < random_number:
+            print("number too low. Try again.")
+        elif guess > random_number:
+            print("Number too high. Try again.")
+
+        
+
+
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ""
+    while feedback != "c":
+        if low != high:
+            guess = random.randint(low, high)
+        guess = random.randint(low, high)
+        feedback = input(f"Is {guess} too high (H), too low (L), or correct (C)?").lower()
+        if feedback == "h":
+            high = guess - 1
+        if feedback == "l":
+            low = guess + 1
+        
+    print(f"Yay the computer guessed {guess} right!")
+
+    computer_guess()
--- madlibs.py
+++ madlibs.py
@@ -1,1 +1,15 @@
-#i did it? 
+# #i did it? 
+# #youtuber = "Bbpanzu"
+# print("sub to " + youtuber)
+# print("sub to {}".format(youtuber))
+# print(f"sub to {youtuber}")
+
+adj = input("Adjective: ")
+verb1 = input("First verb: ")
+verb2 = input("Second verb:")
+famous_person = input("Famous Person: ")
+
+madlib = f"Computer programming is so {adj}! It makes me so excited all the time because I love to {verb1}. \
+ Stay hydrated and {verb2} like you are {famous_person}!"
+
+print(madlib)
